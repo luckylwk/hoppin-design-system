c77f5d8877d730ac3c090a2c68c3c00825b84e89
jonashaefele <hello@jonashaefele.com>
2019-06-13 11:31:51 +0100

    add flex/box components

--- src/components/Box/Box.js
+++ src/components/Box/Box.js
@@ -1,7 +1,16 @@
 import React from 'react';
 import styled from 'styled-components';
-import { space, border, color, layout, textAlign, flexGrow } from 'styled-system';
-import propTypes from '@styled-system/prop-types'
+import {
+  space,
+  border,
+  color,
+  layout,
+  textAlign,
+  flexGrow,
+  flexShrink,
+  flex,
+} from 'styled-system';
+import propTypes from '@styled-system/prop-types';
 
 const Box = styled('div')(
   {
@@ -11,28 +20,29 @@ const Box = styled('div')(
   space,
   layout,
   flexGrow,
+  flexShrink,
+  flex,
   color,
   border,
-  textAlign,
+  textAlign
 );
 
 Box.propTypes = {
   ...propTypes.space,
   ...propTypes.layout,
   ...propTypes.flexGrow,
+  ...propTypes.flexShrink,
+  ...propTypes.flex,
   ...propTypes.color,
   ...propTypes.border,
   ...propTypes.textAlign,
-}
+};
 
 Box.defaultProps = {
   display: 'block',
-  flexGrow: 1
-}
+  flexGrow: 1,
+};
 
-Box.displayName = 'Box'
-
-
-console.log(Box)
+Box.displayName = 'Box';
 
 export default Box;
