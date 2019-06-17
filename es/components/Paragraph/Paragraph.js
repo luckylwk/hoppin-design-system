651aaab8307e53f809e03f3ce6acfb3af9539827
jonashaefele <hello@jonashaefele.com>
2019-06-17 11:35:34 +0100

    allow paragraph to have textAlign

--- es/components/Paragraph/Paragraph.js
+++ es/components/Paragraph/Paragraph.js
@@ -1,12 +1,12 @@
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _templateObject = _taggedTemplateLiteralLoose(['\n  font-size: ', ';\n  font-family: ', ';\n  line-height: ', ';\n  ', '\n  ', '\n'], ['\n  font-size: ', ';\n  font-family: ', ';\n  line-height: ', ';\n  ', '\n  ', '\n']);
+var _templateObject = _taggedTemplateLiteralLoose(['\n  font-size: ', ';\n  font-family: ', ';\n  line-height: ', ';\n  ', '\n  ', '\n  ', '\n'], ['\n  font-size: ', ';\n  font-family: ', ';\n  line-height: ', ';\n  ', '\n  ', '\n  ', '\n']);
 
 function _taggedTemplateLiteralLoose(strings, raw) { strings.raw = raw; return strings; }
 
 import React from 'react';
 import styled from 'styled-components';
-import { space, color } from 'styled-system';
+import { space, color, textAlign } from 'styled-system';
 import propTypes from '@styled-system/prop-types';
 
 var Paragraph = styled('p')(_templateObject, function (_ref) {
@@ -18,12 +18,13 @@ var Paragraph = styled('p')(_templateObject, function (_ref) {
 }, function (_ref3) {
   var theme = _ref3.theme;
   return theme.lineHeights.body || '1.4em';
-}, space, color);
+}, textAlign, space, color);
 
-Paragraph.propTypes = _extends({}, propTypes.space, propTypes.color);
+Paragraph.propTypes = _extends({}, propTypes.space, propTypes.color, propTypes.textAlign);
 
 Paragraph.defaultProps = {
   marginBottom: 3,
-  color: 'inherit' // respond to context, if we're white on dark background or dark on light.
+  color: 'inherit', // respond to context, if we're white on dark background or dark on light.
+  textAlign: 'left'
 };
 export default Paragraph;
--- src/components/Paragraph/Paragraph.js
+++ src/components/Paragraph/Paragraph.js
@@ -1,12 +1,13 @@
 import React from 'react';
 import styled from 'styled-components';
-import { space, color } from 'styled-system';
+import { space, color, textAlign } from 'styled-system';
 import propTypes from '@styled-system/prop-types';
 
 const Paragraph = styled('p')`
-  font-size: ${({theme}) => theme.fontSizes.body || '1em'};
-  font-family: ${({theme}) => theme.fonts.secondary || 'sans-serif'};
-  line-height: ${({theme}) => theme.lineHeights.body || '1.4em'};
+  font-size: ${({ theme }) => theme.fontSizes.body || '1em'};
+  font-family: ${({ theme }) => theme.fonts.secondary || 'sans-serif'};
+  line-height: ${({ theme }) => theme.lineHeights.body || '1.4em'};
+  ${textAlign}
   ${space}
   ${color}
 `;
@@ -14,10 +15,12 @@ const Paragraph = styled('p')`
 Paragraph.propTypes = {
   ...propTypes.space,
   ...propTypes.color,
+  ...propTypes.textAlign,
 };
 
 Paragraph.defaultProps = {
   marginBottom: 3,
-  color: 'inherit' // respond to context, if we're white on dark background or dark on light.
-}
+  color: 'inherit', // respond to context, if we're white on dark background or dark on light.
+  textAlign: 'left',
+};
 export default Paragraph;
