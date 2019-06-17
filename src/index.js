c9a3b20749c57b2605ff5907c523c00fb4b425d7
jonashaefele <hello@jonashaefele.com>
2019-06-17 11:22:39 +0100

    export caption and tags.

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+export { HoppinDesignProvider } from './components/HoppinDesignProvider';
+
 export { Box } from './components/Box';
 export { Button } from './components/Button';
 export { Flex } from './components/Flex';
+
 export { Heading } from './components/Heading';
-export { HoppinDesignProvider } from './components/HoppinDesignProvider';
 export { Paragraph } from './components/Paragraph';
 export { Text } from './components/Text';
+export { Caption } from './components/Caption';
+export { Tag } from './components/Tag';
+
 export { Timeline } from './components/Timeline';
 
 export { default as tokens } from './tokens';
