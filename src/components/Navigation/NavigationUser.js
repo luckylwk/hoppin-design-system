f31506606d65d6bcf2e444940fce1ea707e81989
luckylwk <lrwderksen@gmail.com>
2019-12-03 10:26:39 -0500

    Created NavigationUser component for use in NavigationBar.

---
+++ src/components/Navigation/NavigationUser.js
@@ -0,0 +1,36 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+
+import { Flex } from '../Flex';
+import { Box } from '../Box';
+import { Text } from '../Text';
+import { Avatar } from '../Image';
+
+const NavigationUser = ({ user }) => (
+  <Flex alignItems="center" justifyContent="center">
+    <Box flexGrow={0}>
+      {user.profile.image_url && user.profile.image_url !== '' && (
+        <Avatar
+          variant="icon"
+          src={user.profile.image_url}
+          display="inline-block"
+        />
+      )}
+    </Box>
+    <Box flexGrow={1} paddingLeft="small">
+      <Text fontWeight="bold" display="block">
+        {user.profile.first_name} {user.profile.last_name}
+      </Text>
+    </Box>
+  </Flex>
+);
+
+NavigationUser.propTypes = {
+  user: PropTypes.object.isRequired,
+};
+
+NavigationUser.defaultProps = {};
+
+NavigationUser.displayName = 'NavigationUser';
+
+export default NavigationUser;
--- src/components/Navigation/index.js
+++ src/components/Navigation/index.js
@@ -5,5 +5,5 @@ export {
   NavRight,
 } from './NavigationBar';
 export { default as Navigation, NavToggle } from './Navigation';
-export { default as UserNavigationBar } from './UserNavigationBar';
+export { default as NavigationUser } from './NavigationUser';
 export * from './Menu';
