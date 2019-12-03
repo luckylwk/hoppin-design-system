8ddea423ded3a84df2245147f89b5d414c13dbfd
luckylwk <lrwderksen@gmail.com>
2019-12-03 10:26:52 -0500

    Build.

--- es/components/Image/Avatar.js
+++ es/components/Image/Avatar.js
@@ -1,6 +1,6 @@
 var _extends = Object.assign || function (target) { for (var i = 1; i < arguments.length; i++) { var source = arguments[i]; for (var key in source) { if (Object.prototype.hasOwnProperty.call(source, key)) { target[key] = source[key]; } } } return target; };
 
-var _templateObject = _taggedTemplateLiteralLoose(['\n  width: ', ';\n  height: ', ';\n'], ['\n  width: ', ';\n  height: ', ';\n']);
+var _templateObject = _taggedTemplateLiteralLoose(['\n  ', '\n'], ['\n  ', '\n']);
 
 function _objectWithoutProperties(obj, keys) { var target = {}; for (var i in obj) { if (keys.indexOf(i) >= 0) continue; if (!Object.prototype.hasOwnProperty.call(obj, i)) continue; target[i] = obj[i]; } return target; }
 
@@ -9,46 +9,40 @@ function _taggedTemplateLiteralLoose(strings, raw) { strings.raw = raw; return s
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { variant } from 'styled-system';
 
 import { Box } from '../Box';
 import Tile from './Tile';
 
-var SIZES = {
-  small: '24px',
-  icon: '32px',
-  base: '96px'
-};
+var Wrapper = styled(Box)(_templateObject, variant({
+  variants: {
+    small: { width: '24px', height: '24px' },
+    icon: { width: '32px', height: '32px' },
+    base: { width: '96px', height: '96px' }
+  }
+}));
 
-var Wrapper = styled(Box)(_templateObject, function (_ref) {
-  var size = _ref.size;
-  return SIZES[size] ? SIZES[size] : SIZES['base'];
-}, function (_ref2) {
-  var size = _ref2.size;
-  return SIZES[size] ? SIZES[size] : SIZES['base'];
-});
-
-var Avatar = function Avatar(_ref3) {
-  var src = _ref3.src,
-      size = _ref3.size,
-      squared = _ref3.squared,
-      rest = _objectWithoutProperties(_ref3, ['src', 'size', 'squared']);
+var Avatar = function Avatar(_ref) {
+  var src = _ref.src,
+      size = _ref.size,
+      squared = _ref.squared,
+      rest = _objectWithoutProperties(_ref, ['src', 'size', 'squared']);
 
   return React.createElement(
     Wrapper,
     _extends({ size: size }, rest),
-    React.createElement(Tile, { src: src, ratio: '1/1', borderRadius: squared ? '12px' : '50%' })
+    React.createElement(Tile, { src: src, variant: '1/1', borderRadius: squared ? '12px' : '50%' })
   );
 };
 
 Avatar.propTypes = process.env.NODE_ENV !== "production" ? {
   src: PropTypes.string.isRequired,
-  size: PropTypes.string,
-  squared: PropTypes.bool,
-  display: PropTypes.string
+  variant: PropTypes.string,
+  squared: PropTypes.bool
 } : {};
 
 Avatar.defaultProps = {
-  size: 'default',
+  variant: 'default',
   squared: false,
   display: 'inline-block'
 };
--- es/components/Navigation/index.js
+++ es/components/Navigation/index.js
@@ -1,4 +1,4 @@
 export { default as NavigationBar, NavLeft, NavCenter, NavRight } from './NavigationBar';
 export { default as Navigation, NavToggle } from './Navigation';
-export { default as UserNavigationBar } from './UserNavigationBar';
+export { default as NavigationUser } from './NavigationUser';
 export * from './Menu';
