ad2c61a7100461689ec993550341e138848e16f9
luckylwk <lrwderksen@gmail.com>
2019-12-03 10:25:59 -0500

    Changed Image components to variant from styled-system

--- src/components/Image/Avatar.js
+++ src/components/Image/Avatar.js
@@ -1,36 +1,35 @@
 import React from 'react';
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { variant } from 'styled-system';
 
 import { Box } from '../Box';
 import Tile from './Tile';
 
-const SIZES = {
-  small: '24px',
-  icon: '32px',
-  base: '96px',
-};
-
 const Wrapper = styled(Box)`
-  width: ${({ size }) => (SIZES[size] ? SIZES[size] : SIZES['base'])};
-  height: ${({ size }) => (SIZES[size] ? SIZES[size] : SIZES['base'])};
+  ${variant({
+    variants: {
+      small: { width: '24px', height: '24px' },
+      icon: { width: '32px', height: '32px' },
+      base: { width: '96px', height: '96px' },
+    },
+  })}
 `;
 
 const Avatar = ({ src, size, squared, ...rest }) => (
   <Wrapper size={size} {...rest}>
-    <Tile src={src} ratio="1/1" borderRadius={squared ? '12px' : '50%'} />
+    <Tile src={src} variant="1/1" borderRadius={squared ? '12px' : '50%'} />
   </Wrapper>
 );
 
 Avatar.propTypes = {
   src: PropTypes.string.isRequired,
-  size: PropTypes.string,
+  variant: PropTypes.string,
   squared: PropTypes.bool,
-  display: PropTypes.string,
 };
 
 Avatar.defaultProps = {
-  size: 'default',
+  variant: 'default',
   squared: false,
   display: 'inline-block',
 };
--- src/components/Image/Tile.js
+++ src/components/Image/Tile.js
@@ -1,13 +1,8 @@
 import PropTypes from 'prop-types';
 import styled from 'styled-components';
+import { variant } from 'styled-system';
 
 import { Box } from '../Box';
-
-const ASPECT_RATIOS = {
-  '3/2': { paddingTop: '66%' },
-  '4/3': { paddingTop: '75%' },
-  '1/1': { paddingTop: '100%' },
-};
 
 const Tile = styled(Box)`
   /* 
@@ -15,25 +10,33 @@ const Tile = styled(Box)`
     width and adjusts the height accordingly 
   */
   width: 100%;
-  padding-top: ${({ ratio }) => ASPECT_RATIOS[ratio].paddingTop};
 
-  background-color: ${({ theme }) => theme.colors.neutral.dark};
+  ${variant({
+    variants: {
+      '1/1': { paddingTop: '100%' },
+      '3/2': { paddingTop: '66%' },
+      '4/3': { paddingTop: '75%' },
+    },
+  })}
+
   ${({ src }) =>
     src && src !== ''
       ? `background-image: url(${src});`
-      : ''} background-size: cover;
+      : ''} 
+  background-size: cover;
   background-position: center;
 `;
 
 Tile.propTypes = {
   src: PropTypes.string.isRequired,
-  ratio: PropTypes.string,
+  variant: PropTypes.string,
   borderRadius: PropTypes.string,
 };
 
 Tile.defaultProps = {
-  ratio: '1/1',
+  variant: '1/1',
   borderRadius: '3px',
+  bg: 'neutral.dark',
 };
 
 Tile.displayName = 'Tile';
