bb37b3c87abdf33057d5747f15bc2884b990c59c
Jonas Haefele <hello@jonashaefele.com>
2020-02-05 17:27:29 +0000

    update: use borderWidths for Card as well

--- src/tokens/borders.js
+++ src/tokens/borders.js
@@ -1,8 +1,8 @@
-const borderWidths = ['0px', '1px', '2px', '4px'];
+const borderWidths = ['0', '1px', '2px', '4px'];
 
 borderWidths.none = borderWidths[0];
-borderWidths.light = borderWidths[1];
+borderWidths.small = borderWidths[1];
 borderWidths.base = borderWidths[2];
-borderWidths.heavy = borderWidths[3];
+borderWidths.large = borderWidths[3];
 
 export { borderWidths };
